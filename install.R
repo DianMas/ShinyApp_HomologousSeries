93488af7e33939a2967972f57d55c97ceeb17efa
Steffen Neumann <sneumann@ipb-halle.de>
2021-02-18 11:52:42 +0100

    Started Dockerised app

---
+++ install.R
@@ -0,0 +1,15 @@
+## install.packages("devtools")
+## library("devtools")
+
+## install.packages("BiocManager")
+## install.packages("testthat")
+
+
+install.packages("openssl")
+install.packages("httr")
+install.packages("xml2")
+
+install.packages("tidyverse")
+install.packages("plotly")
+
+
